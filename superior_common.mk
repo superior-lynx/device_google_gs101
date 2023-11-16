c8aa8d0f981dbde4d93a20a681579f86b6edcc3a
Hyper <hyper@remainsilent.net>
2023-11-16 08:52:09 +0530

    Revert "Build Lineage Health HAL"
    This reverts commit f763ddd1f502cb846626c9acf97e45ea7eb65128.

--- superior_common.mk
+++ superior_common.mk
@@ -16,8 +16,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
-
 # Touch
 include hardware/google/pixel/touch/device.mk
